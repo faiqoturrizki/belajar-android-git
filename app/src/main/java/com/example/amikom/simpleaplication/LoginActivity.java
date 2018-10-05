eb29352addc82936925e6aefdcc503776ba83f1a
faiqoturrizki <rizkyfaix7@gmail.com>
2018-10-05 08:38:04 +0700

    belajar pertemuan 3

--- app/src/main/java/com/example/amikom/simpleaplication/LoginActivity.java
+++ app/src/main/java/com/example/amikom/simpleaplication/LoginActivity.java
@@ -1,13 +1,37 @@
 package com.example.amikom.simpleaplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.amikom.simpleaplication.util.PreferencesHelper;
 
 public class LoginActivity extends AppCompatActivity {
+    PreferencesHelper instance;
+    private EditText nama;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        instance = PreferencesHelper.getInstance(getApplicationContext());
+        nama = (EditText) findViewById(R.id.text1);
+        password = (EditText) findViewById(R.id.text2);
+
+    }
+
+    public void masuk(View view) {
+        String n = nama.getText().toString();
+        String p = password.getText().toString();
+        Toast.makeText(getApplicationContext(),"Berhasil Login",Toast.LENGTH_SHORT).show();
+        instance.setLogin(true);
+        instance.setName(n);
+
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
